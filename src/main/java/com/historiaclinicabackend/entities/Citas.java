81a24231f449dbd4024bdb8c375ebbb90d63d9b7
David-593 <andresdavidm759@gmail.com>
2025-04-10 13:42:00 -0500

    Entidad Citas

---
+++ src/main/java/com/historiaclinicabackend/entities/Citas.java
@@ -0,0 +1,154 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.historiaclinicabackend.entities;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author andre
+ */
+@Entity
+@Table(name = "citas")
+@NamedQueries({
+    @NamedQuery(name = "Citas.findAll", query = "SELECT c FROM Citas c"),
+    @NamedQuery(name = "Citas.findByCitaId", query = "SELECT c FROM Citas c WHERE c.citaId = :citaId"),
+    @NamedQuery(name = "Citas.findByCitaFecha", query = "SELECT c FROM Citas c WHERE c.citaFecha = :citaFecha"),
+    @NamedQuery(name = "Citas.findByCitaHora", query = "SELECT c FROM Citas c WHERE c.citaHora = :citaHora"),
+    @NamedQuery(name = "Citas.findByCitaEstado", query = "SELECT c FROM Citas c WHERE c.citaEstado = :citaEstado")})
+public class Citas implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "cita_id")
+    private Integer citaId;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "cita_fecha")
+    @Temporal(TemporalType.DATE)
+    private Date citaFecha;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "cita_hora")
+    @Temporal(TemporalType.TIME)
+    private Date citaHora;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "cita_estado")
+    private String citaEstado;
+    @JoinColumn(name = "cita_cedula_medico", referencedColumnName = "med_cedula_usuario")
+    @ManyToOne
+    private Medicos citaCedulaMedico;
+    @JoinColumn(name = "cita_cedula_paciente", referencedColumnName = "pac_cedula_usuario")
+    @ManyToOne
+    private Pacientes citaCedulaPaciente;
+
+    public Citas() {
+    }
+
+    public Citas(Integer citaId) {
+        this.citaId = citaId;
+    }
+
+    public Citas(Integer citaId, Date citaFecha, Date citaHora, String citaEstado) {
+        this.citaId = citaId;
+        this.citaFecha = citaFecha;
+        this.citaHora = citaHora;
+        this.citaEstado = citaEstado;
+    }
+
+    public Integer getCitaId() {
+        return citaId;
+    }
+
+    public void setCitaId(Integer citaId) {
+        this.citaId = citaId;
+    }
+
+    public Date getCitaFecha() {
+        return citaFecha;
+    }
+
+    public void setCitaFecha(Date citaFecha) {
+        this.citaFecha = citaFecha;
+    }
+
+    public Date getCitaHora() {
+        return citaHora;
+    }
+
+    public void setCitaHora(Date citaHora) {
+        this.citaHora = citaHora;
+    }
+
+    public String getCitaEstado() {
+        return citaEstado;
+    }
+
+    public void setCitaEstado(String citaEstado) {
+        this.citaEstado = citaEstado;
+    }
+
+    public Medicos getCitaCedulaMedico() {
+        return citaCedulaMedico;
+    }
+
+    public void setCitaCedulaMedico(Medicos citaCedulaMedico) {
+        this.citaCedulaMedico = citaCedulaMedico;
+    }
+
+    public Pacientes getCitaCedulaPaciente() {
+        return citaCedulaPaciente;
+    }
+
+    public void setCitaCedulaPaciente(Pacientes citaCedulaPaciente) {
+        this.citaCedulaPaciente = citaCedulaPaciente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (citaId != null ? citaId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Citas)) {
+            return false;
+        }
+        Citas other = (Citas) object;
+        if ((this.citaId == null && other.citaId != null) || (this.citaId != null && !this.citaId.equals(other.citaId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.historiaclinica.entities.Citas[ citaId=" + citaId + " ]";
+    }
+    
+}
