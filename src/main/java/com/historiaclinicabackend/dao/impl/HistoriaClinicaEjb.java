0e3d9f4234b1ebb49ad018c630c941ed4892fede
David-593 <andresdavidm759@gmail.com>
2025-04-21 17:25:17 -0500

    Modificacion del ejb de historia clinica

--- src/main/java/com/historiaclinicabackend/dao/impl/HistoriaClinicaEjb.java
+++ src/main/java/com/historiaclinicabackend/dao/impl/HistoriaClinicaEjb.java
@@ -1,12 +1,13 @@
-
 package com.historiaclinicabackend.dao.impl;
 
 import com.historiaclinicabackend.dao.itf.IHistoriaClinicaEjb;
-import com.historiaclinicabackend.entities.Medicos;
+import com.historiaclinicabackend.entities.HistoriaClinica;
 import com.historiaclinicabackend.entities.Usuarios;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
 
 /**
  *
@@ -15,23 +16,37 @@ import jakarta.persistence.PersistenceContext;
 @Stateless(name = "EjbHistoriaClinica")
 public class HistoriaClinicaEjb implements IHistoriaClinicaEjb {
 
-    @PersistenceContext(name="historiPU")
+    @PersistenceContext(name = "historiPU")
     private EntityManager em;
-    
-    @Override
-    public Medicos createHistory(Medicos medico) throws Exception {
-        em.persist(medico);
-        return medico;
-    }
 
     @Override
-    public Medicos existMedByCedula(Medicos medico) throws Exception {
-        return em.find(Medicos.class, medico.getMedCedulaUsuario());
+    public HistoriaClinica createHistory(HistoriaClinica medicalHistory) throws Exception {
+        em.persist(medicalHistory);
+        return medicalHistory;
     }
 
     @Override
     public Usuarios existUserByCedula(Usuarios usuario) throws Exception {
         return em.find(Usuarios.class, usuario.getUsuCedula());
     }
-    
+
+    @Override
+    public List<HistoriaClinica> findHistoryByCedulaUsuario(Usuarios usuario) throws Exception {
+        TypedQuery<HistoriaClinica> query = em.createQuery(
+                "SELECT h FROM HistoriaClinica h WHERE h.histCedulaUsuario.usuCedula = :cedula",
+                HistoriaClinica.class
+        );
+        query.setParameter("cedula", usuario.getUsuCedula());
+        return query.getResultList();
+    }
+
+    @Override
+    public void deleteHistoriaCli(HistoriaClinica medicalHistory) throws Exception {
+        if (medicalHistory != null) {
+            em.remove(medicalHistory);
+        } else {
+            throw new Exception("Historia clínica no encontrada para eliminación.");
+        }
+    }
+
 }
