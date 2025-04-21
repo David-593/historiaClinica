4a42aa2a01327573584cf86d8e8699ddab315f1d
David-593 <andresdavidm759@gmail.com>
2025-04-21 15:32:43 -0500

    Dao para historia clinica

---
+++ src/main/java/com/historiaclinicabackend/dao/itf/IHistoriaClinicaEjb.java
@@ -0,0 +1,21 @@
+
+package com.historiaclinicabackend.dao.itf;
+
+import com.historiaclinicabackend.entities.Medicos;
+import com.historiaclinicabackend.entities.Usuarios;
+
+/**
+ *
+ * @author andre
+ */
+public interface IHistoriaClinicaEjb {
+    
+    Medicos createHistory(Medicos medico) throws Exception;
+    
+    Medicos getHistoryFromPac(Medicos medico) throws Exception;
+    
+    Medicos existMedById(Medicos medico) throws Exception;
+    
+    Usuarios existUserById(Usuarios usuario) throws Exception;
+    
+}
