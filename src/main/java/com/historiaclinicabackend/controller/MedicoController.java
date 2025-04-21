43a8e367f7e8490533459d710d41d43837c6a836
David-593 <andresdavidm759@gmail.com>
2025-04-21 17:24:58 -0500

    Modificacion de controlador de medico

--- src/main/java/com/historiaclinicabackend/controller/MedicoController.java
+++ src/main/java/com/historiaclinicabackend/controller/MedicoController.java
@@ -7,7 +7,6 @@ import jakarta.inject.Inject;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -48,7 +47,7 @@ public class MedicoController {
         }
     }
 
-    @GET
+    @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/getMedico")
