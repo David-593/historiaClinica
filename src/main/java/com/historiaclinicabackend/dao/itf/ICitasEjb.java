b9c25ee389ab83be9c94a148087d9aa303f75f6c
David-593 <andresdavidm759@gmail.com>
2025-04-29 08:33:01 -0500

    Implementacion de interface para el dao Citas

---
+++ src/main/java/com/historiaclinicabackend/dao/itf/ICitasEjb.java
@@ -0,0 +1,20 @@
+
+package com.historiaclinicabackend.dao.itf;
+
+import com.historiaclinicabackend.entities.Citas;
+import com.historiaclinicabackend.entities.Medicos;
+import com.historiaclinicabackend.entities.Pacientes;
+
+/**
+ *
+ * @author andre
+ */
+public interface ICitasEjb {
+    
+    Citas createCitas(Citas cita) throws Exception;
+    
+    Pacientes existPacByCed(Pacientes paciente) throws Exception;
+    
+    Medicos existMedByCed(Medicos medico) throws Exception;
+    
+}
